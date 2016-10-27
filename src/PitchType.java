0747e03a91f86a7648d4251b31950f6b3eb2de77
EVAN <johnson.ev@husky.neu.edu>
2016-10-27 15:38:21 -0400

    Added PitchType enum to represent the pitch of a Note

---
+++ src/PitchType.java
@@ -0,0 +1,40 @@
+/**
+ * Enumeration for the 12 distant types of pitches.
+ */
+public enum PitchType {
+  C("C", 0), CSharp("C#", 1), D("D", 2), DSharp("D#", 3), E("E", 4), F("F", 5), FSharp("F#", 6),
+  G("G", 7), GSharp("G#", 8), A("A", 9), ASharp("A#", 10), B("B", 11);
+
+  private final String value;
+  private final int order;
+
+  /**
+   * Constructs a {@code PitchType} object.
+   *
+   * @param value the String value of the PitchType
+   * @param order the integer value of the PitchType
+   */
+  PitchType(String value, int order) {
+    this.value = value;
+    this.order = order;
+  }
+
+  /**
+   * Gets the String value of PitchType.
+   */
+  public String getToneValue() {
+    return this.value;
+  }
+
+  /**
+   * Gets the integer order of PitchType.
+   */
+  public int getToneOrder() {
+    return this.order;
+  }
+
+  @Override
+  public String toString() {
+    return this.getToneValue();
+  }
+}
