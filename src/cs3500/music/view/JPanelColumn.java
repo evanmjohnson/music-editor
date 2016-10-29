95b9bacf259d36ad867b3f20ed37315fa6967dda
Shravali <shravali@gmail.com>
2016-10-29 19:06:13 -0400

    Updated JPanel implementation; added columns and note instance classes.

---
+++ src/cs3500/music/view/JPanelColumn.java
@@ -0,0 +1,10 @@
+package cs3500.music.view;
+
+import javax.swing.*;
+
+/**
+ * Represents column panels of every beat in the visual music editor GUI.
+ */
+public class JPanelColumn extends JPanel{
+
+}
---
+++ src/cs3500/music/view/JPanelNoteInstance.java
@@ -0,0 +1,11 @@
+package cs3500.music.view;
+
+import javax.swing.*;
+
+/**
+ * Represents one note at one beat inside a JPanelColumn in the music editor GUI.
+ */
+public class JPanelNoteInstance extends JPanel {
+
+  //starts, continues, or doesn't exist at beat
+}
