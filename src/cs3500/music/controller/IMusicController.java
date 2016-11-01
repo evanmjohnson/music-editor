b65cf6cf00a49424a0982fef14ab176a07660239
EVAN <johnson.ev@husky.neu.edu>
2016-11-01 19:31:28 -0400

    Created interface for controller

--- src/cs3500/music/controller/IMusicController.java
+++ src/cs3500/music/controller/IMusicController.java
@@ -1,14 +1,15 @@
 package cs3500.music.controller;
 
 import cs3500.music.model.IMusicModel;
+import cs3500.music.view.IMusicView;
 
 /**
  * This is the interface for the controller of the music editor.
  */
 public interface IMusicController {
   /**
-   * Start the recording of the given model.
-   * @param model The model to start the recording of
+   * Start the music editor with the given view.
+   * @param viewType The type of view to start the editor with
    */
-  void startRecording(IMusicModel model);
+  void start(IMusicModel model, String viewType);
 }
