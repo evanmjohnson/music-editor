faabfef5ac0bebfff628bb3aaef336d8096b81f1
EVAN <johnson.ev@husky.neu.edu>
2016-11-08 12:57:19 -0500

    Updated Line to have two coordinates instead of two integers

--- src/cs3500/music/view/Line.java
+++ src/cs3500/music/view/Line.java
@@ -7,12 +7,16 @@ import java.util.Objects;
  * Created by blerner on 10/10/16.
  */
 public final class Line {
-  public final int start;
-  public final int end;
+  public final int x0;
+  public final int y0;
+  public final int x1;
+  public final int y1;
 
-  public Line(int start, int end) {
-    this.start = start;
-    this.end = end;
+  public Line(int x0, int y0, int x1, int y1) {
+    this.x0 = x0;
+    this.y0 = y0;
+    this.x1 = x1;
+    this.y1 = y1;
   }
 }
 
