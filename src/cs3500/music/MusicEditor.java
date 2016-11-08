bdaca45ce9dd4504e03a9f7db42b77ceca33a373
Shravali <shravali@gmail.com>
2016-11-08 12:50:42 -0500

    Added implementation for a Line to draw horizontal and vertical lines in the NotesPanel.

--- src/cs3500/music/MusicEditor.java
+++ src/cs3500/music/MusicEditor.java
@@ -17,9 +17,9 @@ public class MusicEditor {
     IMusicModel viewModel;
     args = new String[2];
     args[0] = "midi";
-    args[1] = "/Users/evan/Downloads/code 4/mary-little-lamb.txt";
+    //args[1] = "/Users/evan/Downloads/code 4/mary-little-lamb.txt";
     //args[1] = "/Users/evan/Downloads/code 4/lnl.txt";
-    //args[1] = "/Users/Shravali/Desktop/OOD/starteractual/mary-little-lamb.txt";
+    args[1] = "/Users/Shravali/Desktop/OOD/starteractual/mary-little-lamb.txt";
     viewModel = MusicReader.parseFile(new FileReader(args[1]), new MusicBuilder());
     MusicController controller = new MusicController();
     //controller.start(viewModel, args);
