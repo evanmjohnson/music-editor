95eb51004db729d8801c334553a1ea966268efa4
EVAN <johnson.ev@husky.neu.edu>
2016-11-08 11:42:02 -0500

    Changed visual view implementation

--- src/cs3500/music/view/JFrameView.java
+++ src/cs3500/music/view/JFrameView.java
@@ -2,6 +2,9 @@ package cs3500.music.view;
 
 import cs3500.music.model.MusicViewModel;
 import cs3500.music.model.Note;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import javax.swing.*;
@@ -15,16 +18,14 @@ public class JFrameView extends JFrame implements IMusicView {
 
   private int measureLength;
   private int numBeats;
-  private JLabel display;
-  private JPanelColumn col;
+  private JPanel[][] panels;
 
 
   public JFrameView() {
     super();
-    setPreferredSize(new Dimension(1500, 300));
+    //setPreferredSize(new Dimension(1500, 300));
     setLocation(200, 200);
     setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    this.setLayout(new BorderLayout());
     this.setResizable(false);
   }
 
@@ -35,21 +36,27 @@ public class JFrameView extends JFrame implements IMusicView {
 
   @Override
   public void create(MusicViewModel model) {
-    int cols = model.getNumBeats();
-    int rows = model.getNoteRange().size();
-    this.setPreferredSize(new Dimension(cols * 50, rows * 25));
+    int cols = model.getNumBeats() + 1;
+    int rows = model.getNoteRange().size() + 1;
+    this.panels = new JPanel[rows][cols];
+    for (JPanel[] row : panels) {
+      Arrays.fill(row, new JPanel());
+    }
     JPanel notePanel = new JPanel();
     JPanel beatPanel = new JPanel();
     notePanel.setLayout(new BoxLayout(notePanel, BoxLayout.Y_AXIS));
     notePanel.setBackground(new Color(238, 238, 238));
+    this.setLayout(new BorderLayout());
+    //this.setPreferredSize(new Dimension(cols * 50, rows * 25));
 
     // write the range of notes on the left side
-    for (Note n : model.getNoteRange()) {
+    List<Note> range = model.getNoteRange();
+    for (Note n : range) {
       JLabel label = new JLabel(n.toString());
       label.setFont(new Font("Josephine Sans", Font.PLAIN, 18));
       notePanel.add(label);
-      this.add(notePanel, BorderLayout.WEST);
     }
+    this.add(notePanel, BorderLayout.WEST);
 
     // create each of the beats on the top
     beatPanel.setBackground(new Color(238, 238, 238));
@@ -67,20 +74,44 @@ public class JFrameView extends JFrame implements IMusicView {
         beatPanel.add(Box.createRigidArea(new Dimension(50, 0)));
       }
     }
+    this.add(beatPanel, BorderLayout.NORTH);
 
     // create all of the notes in the middle
     JPanel noteGrid = new JPanel();
-    noteGrid.setLayout(new GridLayout(1, this.numBeats));
+    noteGrid.setLayout(new GridLayout());
     for (int i = 0; i <= numBeats; i++) {
       List<Integer> cont = model.notesContinueAtThisBeat(i);
       List<Integer> start = model.notesStartAtThisBeat(i);
-      col = new JPanelColumn(i, start, cont);
-      col.draw(model);
-      noteGrid.add(col);
+      List<Integer> not = new ArrayList<>();
+      for (int k = 0; k < range.size(); k++) {
+        not.add(k);
+      }
+      for (Integer s : start) {
+        JPanelNoteInstance noteInstance = new JPanelNoteInstance();
+        noteInstance.setColor("start");
+        this.panels[s][i].add(noteInstance);
+        not.remove(s);
+      }
+      for (Integer c : cont) {
+        JPanelNoteInstance noteInstance = new JPanelNoteInstance();
+        noteInstance.setColor("continue");
+        this.panels[c][i].add(noteInstance);
+        not.remove(c);
+      }
+      for (Integer n : not) {
+        JPanelNoteInstance noteInstance = new JPanelNoteInstance();
+        noteInstance.setColor("none");
+        this.panels[n][i].add(noteInstance);
+      }
     }
 
-    this.add(beatPanel, BorderLayout.NORTH);
+    for (JPanel[] row : panels) {
+      for (JPanel cell : row) {
+        noteGrid.add(cell);
+      }
+    }
     this.add(noteGrid, BorderLayout.CENTER);
+
     pack();
   }
 
