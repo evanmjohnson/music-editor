a009e193d30115da4fd0fb9035b5210d84f3a9ee
EVAN <johnson.ev@husky.neu.edu>
2016-11-08 13:48:31 -0500

    Added a class to represent rectangles in the visual view

---
+++ src/cs3500/music/view/Rectangle.java
@@ -0,0 +1,23 @@
+package cs3500.music.view;
+
+import java.awt.*;
+
+/**
+ * Represents a rectangle to draw a Note in the visual view of the music editor. Supports
+ * custom Colors.
+ */
+public class Rectangle {
+  public final Color color;
+  public final int x;
+  public final int y;
+  public final int width;
+  public final int height;
+
+  public Rectangle(int x, int y, int width, int height, Color color) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.color = color;
+  }
+}
