31fa148ff5170195a16cecf55b3eee17dd930a92
EVAN <johnson.ev@husky.neu.edu>
2016-11-12 19:19:58 -0500

    Added starter action listener methods

--- src/cs3500/music/view/IMusicGUIView.java
+++ src/cs3500/music/view/IMusicGUIView.java
@@ -2,6 +2,7 @@ package cs3500.music.view;
 
 import cs3500.music.model.Note;
 
+import java.awt.event.ActionListener;
 import java.util.IllegalFormatException;
 
 /**
@@ -16,5 +17,14 @@ public interface IMusicGUIView extends IMusicView {
    * @throws IllegalFormatException If the input is malformed
    */
   Note showAddPrompt() throws IllegalFormatException;
+
+  /**
+   * Reset the focus on the appropriate part of the view that has the keyboard listener
+   * attached to it, so that keyboard events will still flow through.
+   */
+  void resetFoucs();
+
+  void addActionListener(ActionListener listener);
+
 
 }
