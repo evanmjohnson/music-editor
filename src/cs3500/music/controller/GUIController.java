3619eef975630d5c677adf1c7100f1bc874e048a
EVAN <johnson.ev@husky.neu.edu>
2016-11-16 20:14:48 -0500

    Fixed keyboard listener

--- src/cs3500/music/controller/GUIController.java
+++ src/cs3500/music/controller/GUIController.java
@@ -10,6 +10,7 @@ import cs3500.music.view.JFrameView;
 import javax.swing.*;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -49,63 +50,33 @@ public class GUIController extends MusicController {
     Map<Integer, Runnable> keyPresses = new HashMap<>();
     Map<Integer, Runnable> keyReleases = new HashMap<>();
 
-    //keyPresses.put(KeyEvent.VK_A, new MakeCaps());
-    // keyReleases.put(KeyEvent.VK_C, new MakeOriginalCase());
-    // Another possible syntax: instead of defining a new class, just to make a single instance,
-    // you can create an "anonymous class" that implements a particular interface, by writing
-    // "new Interfacename() { all the methods you need to implement }"
-    // Note that "view" is in scope inside this Runnable!  But, also note that within the Runnable,
-    // "this" refers to the Runnable and not to the Controller, so we don't say "this.view".
-
-    keyTypes.put(KeyEvent.VK_A, () -> {
-      Note n = view.showAddPrompt();
-      model.add(n);
-      //Note inRange = new Note(n.getPitch(), 0, 0, n.getOctave());
-
-      this.view = new JFrameView();
-      MusicViewModel viewModel = new MusicViewModel(model);
-      view.create(viewModel);
-      view.makeVisible();
-      /*
-      if(model.getNoteRange().contains(inRange)
-          && model.getNumBeats() < n.getStartBeat() + n.getDuration()) {
-        view.drawNote(n,  model.getNoteRange().indexOf(inRange));
-      }
-      else {
-        this.view = new JFrameView();
-        MusicViewModel viewModel = new MusicViewModel(model);
-        view.create(viewModel);
-        view.makeVisible();
-      }
-      */
-    });
-    // Another possible syntax:
-    // Instead of an anonymous class, you can (as of Java 8) use "lambda syntax",
-    // as follows: if the interface you want to implement has only one single method,
-    // then write the parenthesized argument list, followed by an arrow, followed by
-    // the body of the method.  Java will infer that you mean to implement the particular
-    // single method of that interface, and translate the code for you to resemble the
-    // anonymous Runnable example above.
-    // Again note all the names that are in scope.
     keyTypes.put(KeyEvent.VK_R, () -> {
-      // exchange the hotkeys C and U:
-      // Take the event handlers from VK_C and VK_U
-      Runnable oldCHandler = keyPresses.get(KeyEvent.VK_C);
-      Runnable oldUHandler = keyPresses.get(KeyEvent.VK_U);
-      // Update the C handler
-      if (oldCHandler != null)
-        keyPresses.put(KeyEvent.VK_U, oldCHandler);
-      else
-        keyPresses.remove(KeyEvent.VK_U);
-      // Update the U handler
-      if (oldUHandler != null)
-        keyPresses.put(KeyEvent.VK_U, oldCHandler);
-      else
-        keyPresses.remove(KeyEvent.VK_U);
+      System.out.println("RIGHT");
+      this.view.scrollRight();
     });
+
+    keyReleases.put(KeyEvent.VK_RIGHT, () -> {
+      System.out.println("RIGHT");
+      this.view.scrollRight();
+    });
+
+    keyTypes.put(KeyEvent.VK_L, () -> {
+      this.view.scrollLeft();
+      System.out.println("LEFT");
+    });
+
+    keyReleases.put(KeyEvent.VK_LEFT, () -> {
+      this.view.scrollLeft();
+      System.out.println("LEFT");
+    });
+
 
     keyReleases.put(KeyEvent.VK_A, () -> {
       Note n = view.showAddPrompt();
+      model.add(n);
+      System.out.println(model.notesStartAtThisBeat(0));
+      MusicViewModel viewModel = new MusicViewModel(model);
+      this.view.reDraw(viewModel);
     });
 
     KeyboardHandler kbd = new KeyboardHandler();
@@ -115,48 +86,13 @@ public class GUIController extends MusicController {
     view.addListener(kbd);
   }
 
-  /*
-  @Override
-  public void actionPerformed(ActionEvent e) {
-    // TODO Auto-generated method stub
-    switch (e.getActionCommand()) {
-      //read from the input textfield
-      case "Echo Button":
-        String text = view.getInputString();
-        //send text to the model
-        model.setString(text);
+  private void configureMouseListener() {
+    Map<Integer, Runnable> mousePressed = new HashMap<>();
+    Map<Integer, Runnable> mouseReleased = new HashMap<>();
+    Map<Integer, Runnable> mouseClicked = new HashMap<>();
 
-        //clear input textfield
-        view.clearInputString();
-        //finally echo the string in view
-        text = model.getString();
-        view.setEchoOutput(text);
+    mousePressed.put(MouseEvent.MOUSE_PRESSED, () -> {
 
-        //set focus back to main frame so that keyboard events work
-        view.resetFocus();
-
-        break;
-      case "Exit Button":
-        System.exit(0);
-        break;
-    }
+    });
   }
-
-  // THESE CLASSES ARE NESTED INSIDE THE CONTROLLER,
-  // SO THAT THEY HAVE ACCESS TO THE VIEW
-  class MakeCaps implements Runnable {
-    public void run() {
-      String text = model.getString();
-      text = text.toUpperCase();
-      view.setEchoOutput(text);
-    }
-  }
-
-  class MakeOriginalCase implements Runnable {
-    public void run() {
-      String text = model.getString();
-      view.setEchoOutput(text);
-    }
-  }
-  */
 }
